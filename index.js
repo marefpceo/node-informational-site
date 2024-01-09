d736c850f0a2c9293d9c4e6b85258e5517e81712
marefpceo <32721274+marefpceo@users.noreply.github.com>
2024-01-09 15:21:10 -0800

    Refactored code to use static method
    Created public folder and moved all static files it.

--- index.js
+++ index.js
@@ -4,25 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use(express.static(path.join(__dirname, 'public')), (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
-
-app.get('/about.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about.html'));
-});
-
-app.get('/contact-me.html', (req, res) => {
-  res.sendFile(path.join(__dirname, 'contact-me.html'));
-});
-
-
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '404.html'));
-});
-
-
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
